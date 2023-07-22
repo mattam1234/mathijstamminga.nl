8224f1422fe9781bba8091fdcbaeb611046507c2
Mathijs <mattam1921@gmail.com>
2023-07-22 22:15:18 +0200

    getting ready to launch🚀🚀

--- next.config.js
+++ next.config.js
@@ -4,7 +4,12 @@
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
-  output: 'export',
-}
+  output: "export",
+  distDir: "build",
+  experimental: {
+    appDir: true,
+  },
+  reactStrictMode: false,
+};
 
 module.exports = nextConfig
